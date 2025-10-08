7eaa69d7e6b9e4c2fab1cdcce54da5dddf4bed1d
Nerd658 <m9bikienga@gmail.com>
2025-10-08 09:59:17 +0000

    test

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const apiRoutes = require('./src/routes/api');
-
 const app = express();
 const port = process.env.PORT || 3000;
 
