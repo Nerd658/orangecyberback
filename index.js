49cee5af99d6211b836d80b3b0ddb318cbb1e5ec
Nerd658 <m9bikienga@gmail.com>
2025-10-10 10:02:41 +0000

    last change

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -14,6 +13,3 @@ app.use('/api', apiRoutes);
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
-//hel
-SUPABASE_URL=https://eokfpyhrhsibcjpbzret.supabase.co
-DATABASE_URL=postgresql://postgres.eokfpyhrhsibcjpbzret:oGvfIggAP6K06Qhj@aws-1-eu-west-3.pooler.supabase.com:5432/postgres
